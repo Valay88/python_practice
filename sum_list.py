fed1957be7421bf957e02cf82c36fd8d50727682
“valay <valay.pan93@gmail.com>
2025-07-21 16:01:36 +0530

    sum of the given list

---
+++ sum_list.py
@@ -0,0 +1,15 @@
+'''
+l = [1,2,3,4,5,6,7,8,9]
+sum = 45
+total = 0
+for loop i ---> l
+    total = i + total  ---> till the last element loop will run
+    print ---> total
+
+'''
+
+l = [1,2,3,4,5,6,7,8,9]
+total = 0
+for i in l:
+    total = total + i
+    print([total])
